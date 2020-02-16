1d332294713552322c6fede4ffd4c643d56d1d86
Philip Lee-Chin <phil.leechin@outlook.com>
2020-02-16 23:42:34 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,3 +17,9 @@ def take_a_number(array, customer)
   array.push(customer)
   puts "Welcome, #{customer}. You are number #{array.size} in line."
 end
+
+def now_serving(array)
+  if array.size == 0
+    puts "There is nobody waiting to be served!"
+  end
+end
