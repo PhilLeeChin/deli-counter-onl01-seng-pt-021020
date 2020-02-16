3df12bb708a6e05af3c55f461b6263d5d23de59c
Philip Lee-Chin <phil.leechin@outlook.com>
2020-02-16 23:19:26 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,3 +2,13 @@
 def line(katz_deli)
   puts "The line is currently empty."
 end
+
+def line(other_deli)
+  count = 1
+  newarr = []
+  other_deli.each do |i|
+    newarr << ("The line is currently: #{count}. #{i}")
+    count += 1
+  end
+  newarr
+end
