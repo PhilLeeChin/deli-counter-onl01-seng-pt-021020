a1ffb9f29077beb9238175574abe9e1d7d894d6b
Philip Lee-Chin <phil.leechin@outlook.com>
2020-02-16 23:26:17 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,14 +1,12 @@
 # Write your code here.
 def line()
-  puts "The line is currently empty."
-end
-
-def line(other_deli)
-  count = 1
-  newarr = []
-  other_deli.each do |i|
-    newarr << ("The line is currently: #{count}. #{i}")
-    count += 1
+  if katz_deli == 0
+    puts "The line is currently empty."
+  else
+    count = 1
+    newarr = []
+    other_deli.each do |i|
+      newarr << ("The line is currently: #{count}. #{i}")
+      count += 1
   end
-  newarr
 end
