b93a786d414f80aef4bb03d93215b5dab2422301
Philip Lee-Chin <phil.leechin@outlook.com>
2020-02-16 23:12:06 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,4 +1,9 @@
 # Write your code here.
-def line(katz_deli = [])
-  puts "The line is currently empty."
+def line(katz_deli)
+  count = 0
+  if katz_deli.size > 0
+    puts "The line is currently: #{count}."
+    count += 1
+  else
+    puts "The line is currently empty."
 end
