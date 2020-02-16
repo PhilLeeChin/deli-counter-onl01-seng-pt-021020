30e67169396f0095b751039cd4ab85d72a772c91
Philip Lee-Chin <phil.leechin@outlook.com>
2020-02-16 23:39:27 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,8 +14,15 @@ def line(array)
 end
 
 def take_a_number(array, customer)
-count = 1
   if array.size == 0
     puts "There is nobody in line."
+  elsif array.size >= 1
+    count = 1
+    newarr = []
+    array.each do |i|
+      newarr << ("#{count}. #{i}")
+      count += 1
+    end
+    puts "Welcome, #{customer}. You are number #{count} in line."
   end
 end
