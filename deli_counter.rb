f2bcbde31d6f0d417170b6430f1905b13e85a833
Philip Lee-Chin <phil.leechin@outlook.com>
2020-02-16 23:15:00 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,8 +1,10 @@
 # Write your code here.
-def line(katz_deli, other_deli)
+def line
+  katz_deli
+  other_deli
   count = 1
   if katz_deli.size > 0
-    puts "The line is currently: #{count}."
+    puts "The line is currently: #{count}. #{other_deli}"
     count += 1
   else
     puts "The line is currently empty."
