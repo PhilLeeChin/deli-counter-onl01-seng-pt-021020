40327f7c66a77b351a255daaca9d5eb3bf6c14d4
Philip Lee-Chin <phil.leechin@outlook.com>
2020-02-16 23:27:12 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,11 +2,4 @@
 def line()
   if katz_deli == 0
     puts "The line is currently empty."
-  elsif
-    count = 1
-    newarr = []
-    other_deli.each do |i|
-      newarr << ("The line is currently: #{count}. #{i}")
-      count += 1
-  end
 end
